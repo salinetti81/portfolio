4aa43d2020f7688422b2a3036ebb1c9a840b2b70
Daniel Salinetti <dansalinetti@gmail.com>
2016-09-26 17:36:46 -0400

    Updating skills

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@
 var contactform =  document.getElementById('contactform');
 contactform.setAttribute('action', '//formspree.io/' + 'dansalinetti' + '@' + 'gmail' + '.' + 'com');
 
+//Google Analytics Script
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-83134149-1', 'auto');
+ga('send', 'pageview');
+
 
 $(document).ready(function() {
 
